eefdf0cec25235c46c5113c411bb7a937be6701f
Alona Enraght-Moony <code@alona.page>
2023-07-23 22:16:20 +0100

    Lexicon Generation Script

---
+++ lexgen/update_lexicons.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eoxu pipefail
+
+git clone https://github.com/bluesky-social/atproto.git
+cd atproto/
+
+commit=$(git log -n 1 --pretty='%H' ./lexicons/)
+
+cd ..
+
+rm -rf ./lexicons/
+mv atproto/lexicons/ ./lexicons/
+
+git add ./lexicons/
+
+git commit -m "Update lexicons to commit ${commit}
+
+See https://github.com/bluesky-social/atproto/commit/${commit}
+"
+
+rm -rf atproto/
