3e8c4dc0e9b02c03ba446834f0f0775ced462397
Alona Enraght-Moony <code@alona.page>
2023-08-05 00:46:47 +0100

    Create a post!!
    https://bsky.app/profile/did:plc:mmbnkonsfestr6vvcpy7ffz3/post/3k467z64nk42r

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,8 +1,12 @@
-use chrono::{Local, Utc};
+use chrono::Local;
 use triphosphate::lex::app::bsky::feed::Post;
 
-fn main() {
-    let now = Utc::now();
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let client = reqwest::Client::builder()
+        .user_agent("triphsphate")
+        .build()
+        .unwrap();
 
     let post = Post {
         created_at: Local::now().fixed_offset(),
@@ -11,10 +15,58 @@ fn main() {
         facets: None,
         langs: None,
         reply: None,
-        text: "Hello, world!".to_string(),
+        text: "Hello from Triphosphate!!".to_string(),
     };
 
     let post_json = serde_json::to_string_pretty(&post).unwrap();
 
     println!("{}", post_json);
+
+    let resp = client
+        .post("https://bsky.social/xrpc/com.atproto.server.createSession")
+        .json(&LoginBody {
+            identifier: std::env::var("ATP_USERNAME").unwrap(),
+            password: std::env::var("ATP_PASSWORD").unwrap(),
+        })
+        .send()
+        .await
+        .unwrap();
+
+    dbg!(resp.status());
+    let creds = resp.json::<LoginResponse>().await.unwrap();
+
+    let resp = client
+        .post("https://bsky.social/xrpc/com.atproto.repo.createRecord")
+        .bearer_auth(&creds.access_jwt)
+        .json(&CreateRecord {
+            repo: creds.did,
+            collection: "app.bsky.feed.post".to_string(),
+            record: post,
+        })
+        .send()
+        .await
+        .unwrap();
+
+    dbg!(resp.status());
+    dbg!(resp.text().await.unwrap());
+}
+
+// TODO: Derive these from createSession.json
+#[derive(serde::Serialize)]
+struct LoginBody {
+    identifier: String,
+    password: String,
+}
+#[derive(serde::Deserialize, Debug)]
+struct LoginResponse {
+    #[serde(rename = "accessJwt")]
+    access_jwt: String,
+    did: String,
+}
+
+#[derive(serde::Serialize)]
+struct CreateRecord<T> {
+    repo: String,
+    collection: String,
+    record: T,
 }
