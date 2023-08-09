b2a7112e53a1eb065e39cce8949152cbff946bef
Alona Enraght-Moony <code@alona.page>
2023-08-09 13:11:59 +0100

    Generate a Typescript lexicon validator.

---
+++ lexgen/generate_validator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eoxu pipefail
+
+npm run generate
+# We don't need any of this stuff to call from TS, we just want lexicons.validate.
+rm -rf ./validator/lexicon/types/
+rm ./validator/lexicon/index.ts
+rm ./validator/lexicon/util.ts
