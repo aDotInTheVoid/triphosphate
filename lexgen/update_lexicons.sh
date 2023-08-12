27d6aa5658c07fe986e8137bc3c25ab2331030aa
Alona Enraght-Moony <code@alona.page>
2023-08-12 11:38:53 +0100

    lexgen: Expand `update_lexicons.sh` to run generators.
    Closes #2

--- lexgen/update_lexicons.sh
+++ lexgen/update_lexicons.sh
@@ -15,6 +15,13 @@ curl "https://raw.githubusercontent.com/bluesky-social/atproto/main/LICENSE" > .
 
 git add ./lexicons/
 
+cargo r --bin lexgen
+./generate_validator.sh
+cargo fmt --all
+
+git add ./validator/
+git add ../src/lex/
+
 git commit -m "Update lexicons to commit ${commit}
 
 See https://github.com/bluesky-social/atproto/commit/${commit}
